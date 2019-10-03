e53801eaf81f3924b69ae552fad4db716c16eb1d
A. Noval Adip <a.novaladip@yahoo.com>
2019-10-04 01:28:24 +0700

    add auth provider

---
+++ lib/provider/auth.provider.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:pusyantek/provider/services/auth_service.dart';
+import 'package:pusyantek/provider/services/jwt_service.dart';
+import 'package:pusyantek/utils/api.dart';
+
+class Auth with ChangeNotifier {
+  final _authService = AuthService();
+  final _jwtService = JwtService();
+
+  bool _isAuthenticated = false;
+
+  bool get isAuthenticated => _isAuthenticated;
+
+  Auth() {
+    initializing();
+  }
+
+  Future<void> login(String email, String password) async {
+    try {
+      final jwt = await _authService.login(email, password);
+      await _jwtService.save(jwt);
+      Api.setDefaultAuthHeader(jwt);
+      _isAuthenticated = true;
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> register({
+    @required String email,
+    @required String password,
+    @required String name,
+  }) async {
+    try {
+      await _authService.register(email: email, password: password, name: name);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> logout() async {
+    await _jwtService.removeFromSharedPreferences();
+    _isAuthenticated = false;
+    Api.removeDefaultAuthHeader();
+    notifyListeners();
+  }
+
+  Future<void> initializing() async {
+    try {} catch (error) {
+      throw error;
+    }
+  }
+}
