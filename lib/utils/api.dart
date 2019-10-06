ee6bbba37fe94e6951fb330880dde2b472e67a73
imamrmdn <imamramadhan420@gmail.com>
2019-10-07 03:46:25 +0700

    change url

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 
-final options =
-    BaseOptions(baseUrl: "https://stark-wildwood-18722.herokuapp.com");
+final options = BaseOptions(baseUrl: "https://warm-plains-38887.herokuapp.com");
 
 final api = new Dio(options);
 
